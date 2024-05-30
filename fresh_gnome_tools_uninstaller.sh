233eaf76acdd7de93f22c1c57c6fbb21ed5c8aed
M-Electronics IT <github@mmehn.de>
2024-05-30 19:33:32 +0200

    Add fresh_gnome_tools_uninstaller.sh

---
+++ fresh_gnome_tools_uninstaller.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+apt remove gnome-weather geary gnome-contacts gnome-calendar popsicle popsicle-gtk totem eog simple-scan evince seahorse character-map gnome-calendar gnome-contacts gnome-weather gnome-maps gnome-2048 aisleriot five-or-more evolution gnome-chess four-in-a-row hitori gnome-klotski totem cheese lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel gnome-music gnome-sudoku shotwell gnome-sound-recorder swell-foop tali gnome-taquin gnome-tetravex gnome-robots rhythmbox eog evince simple-scan sane-airscan sane-utils 
+apt autoremove
